50a119c2d7b8e51cc3b5f131b8c4ef351b3c215c
ElizabethMunz <munz@temple.edu>
2019-03-18 14:36:06 -0400

    minor change

--- game/player.js
+++ game/player.js
@@ -4,7 +4,7 @@ class Player {
     /**
      * Player objects have identity and info for each individual player:
      * @property {number} room - the 4-digit room code, same for all players in a game; identifies which game this player is in - this might not be necessary actually, uncertain
-     * @property {boolean} isHuman - true if human player, false if bot player
+     * @property {boolean} isHuman - true if human player, false if bot player - also irrelevant at this point & may not ever be needed, since we should receive same info from all players
      * @property {string} name - the nickname entered by the player when they join the room
      * @property {string} identity - the character identity
      * @property {boolean} onTeamGood - true if on good team, false if on evil team
