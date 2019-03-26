ee3bffe0be085e40e625822bd1deee52dc7554c0
Talal Kerbag <34694615+Talalkerbag@users.noreply.github.com>
2019-03-26 19:10:26 -0400

    Fixed room seperation

---
+++ game/player.js
@@ -0,0 +1,21 @@
+
+class Player {
+
+    /**
+     * Player objects have identity and info for each individual player:
+     * @property {number} room - the 4-digit room code, same for all players in a game; identifies which game this player is in - this might not be necessary actually, uncertain
+     * @property {boolean} isHuman - true if human player, false if bot player - also irrelevant at this point & may not ever be needed, since we should receive same info from all players
+     * @property {string} name - the nickname entered by the player when they join the room
+     * @property {string} identity - the character identity
+     * @property {boolean} onTeamGood - true if on good team, false if on evil team
+     * @property {string[]} knownIdentities - a list of the player names whose team or identity is known to this player
+     */
+
+    /**
+     * constructor for player object
+     */
+    constructor() {
+
+    }
+
+}
