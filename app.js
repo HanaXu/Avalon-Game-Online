a684f3fc4c5bd4f72d4709d62b0b7f79933ffe26
Hanaxu <hanaxu12@gmail.com>
2019-04-03 01:28:43 -0400

    spring cleaning. cleaned create, connect & disconnect code

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ let express = require("express");
 let app = express();
 let serv = require("http").Server(app);
 
+var Game = require("./game/Game");
+var Player = require("./game/Player");
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/client/index.html");
 });
@@ -13,19 +16,18 @@ app.use("/client", express.static(__dirname + "/client"));
 serv.listen(process.env.PORT || 2000);
 console.log("Server started.");
 
-//create three arrays for sockets, game code and players
-const SocketList = [[], [], [], [], []]; //[room index: 0,1,2..] [socket.id]
-const PlayerList = [[], [], [], [], []]; //[room index: 0,1,2..] [socket.id]
-const SocketIDS = [[], [], [], [], []]; //[room index: 0,1,2..] [player index: 0,1,2..]
+// const SocketList = [[], [], [], [], []]; //Contains all sockets of clients
+// const PlayerList = []; //Contains all player objects
 
-const GameIsClosed = {}; //states if the game is open or closed
-const AssignIdentities = {};
-const GameList = {}; // [room index] = the room code
-const PlayerName = {};
-let gameRoomsCount = 0;
+// const PlayerCount = {}; //Keeps record of player count in each game
+const GameIsClosed = {}; //value of 0 or 1 for each game. if 1 room is open, if 0 room is closed
+const GameStage = {}; //keeps record of game stage in each room
 
-// key = number of players
-// value = list of characters
+const GameList = []; //keeps record of all game objects
+
+let gameRoomsCount = 0; //keeps record of how many game rooms there are
+
+//constants used in assignIdentites function depending on how many players are connected when game is started
 const PlayerIdentities = {
   "5": [
     "Merlin",
@@ -86,204 +88,227 @@ const PlayerIdentities = {
   ]
 };
 
-const Good = new Set(["Merlin", "Loyal Servant of Arthur"]);
-const Evil = new Set(["Assassin", "Minion of Mordred"]);
+const GoodTeam = new Set(["Merlin", "Loyal Servant of Arthur"]);
 
-let quest1, quest2, quest3, quest4, quest5;
+// let quest1, quest2, quest3, quest4, quest5;
+// let quests = [quest1, quest2, quest3, quest4, quest5];
 
-let quests = [quest1, quest2, quest3, quest4, quest5];
-
-//takes 3 param and sets players roles based on their length.
-function assignIdentities(numberOfPlayers, roomNumber, socket_id) {
+//assigns each player an identity in the game
+function assignIdentities(numberOfPlayers, roomNumber) {
   console.log("assignIdentities()");
   var shuffledIdentities = shuffle(PlayerIdentities[numberOfPlayers]);
 
+  //x = index of shuffledIdentities array
+  var x = 0;
+  //loops through the player list, if it is not null assign an identity from the shuffled Identities
   for (var i = 0; i < PlayerList[roomNumber].length; i++) {
-    PlayerList[roomNumber][socket_id[i]].character = shuffledIdentities[i];
-    if (Good.has(shuffledIdentities[i])) {
-      PlayerList[roomNumber][socket_id[i]].team = "Good";
-    } else {
-      PlayerList[roomNumber][socket_id[i]].team = "Evil";
+    if (PlayerList[roomNumber][i] != null) {
+      PlayerList[roomNumber][i].character = shuffledIdentities[x];
+      if (GoodTeam.has(shuffledIdentities[x])) {
+        PlayerList[roomNumber][i].team = "Good";
+      } else {
+        PlayerList[roomNumber][i].team = "Evil";
+      }
+      x++;
     }
   }
+}
 
-  //I did not assign any players their identities yet, not sure how the players are being passed
-  AssignIdentities[roomNumber] = 2;
-
-  //fisher-yates algorithm
-  function shuffle(array) {
-    var currentIndex = array.length,
-      temporaryValue,
-      randomIndex;
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+//randomly assign a room leader in the player list.
+function assignLeader(roomNumber) {
+  var randomNumber = Math.floor(
+    Math.random() * Math.floor(PlayerList[roomNumber].length)
+  );
+  for (var i = 0; i < PlayerList[roomNumber].length; i++) {
+    if (PlayerList[roomNumber][i] != null) {
+      PlayerList[roomNumber][i].leader = true;
+      break;
     }
-    return array;
   }
+  GameStage[roomNumber] = 2;
+}
+
+//shuffle the array
+function shuffle(array) {
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
 
 const io = require("socket.io")(serv, {});
 
 //start connection
-io.sockets.on("connection", function(socket) {
-    
+io.on("connection", function(socket) {
+
   //start room connection
   socket.on("roomCode", function(data) {
-    const code = data.code;
-    let roomNumber;
-    let playerPosition;
-    let exist = false;
-    socket.join(data.code);
+    const roomCode = data.roomCode;
+    console.log("got room code: " + roomCode);
+    socket.join(roomCode); //subscribe the socket to the roomcode
 
-    //go through game list and if it exists or open connect the player to a game
+    let roomNumber;
+
+    let roomExists = false;
+    let game;
+    let gameIndex;
+
     for (let i in GameList) {
-      if (GameList[i] === code && GameIsClosed[gameRoomsCount - 1] === 1) {
-        roomNumber = i;
-        playerPosition = PlayerList[i].length;
-        exist = true;
-        console.log(
-          "Connected to Game: " +
-            code +
-            " room number: " +
-            roomNumber +
-            " player position: " +
-            playerPosition
-        );
+      if (GameList[i].roomCode === roomCode) {
+        console.log("room already exists");
+        roomExists = true;
+        gameIndex = i;
         break;
       }
     }
+    if (!roomExists) {
+      console.log("room does not exist. creating new game room");
+      game = new Game(roomCode);
+    }
 
-    //if game does not exist add game stuff
-    if (!exist) {
-      AssignIdentities[gameRoomsCount] = 0;
-      GameIsClosed[gameRoomsCount] = 1;
-      GameList[gameRoomsCount] = code;
-      roomNumber = gameRoomsCount;
-      playerPosition = 0;
-      gameRoomsCount = gameRoomsCount + 1;
-      console.log(
-        "Created New Game: " +
-          code +
-          " room number: " +
-          roomNumber +
-          " player position: " +
-          playerPosition
+    console.log("connected to room: " + roomCode + "\n");
+    var player;
+    var name;
+
+    socket.on("playerName", function(data) {
+      name = data.name;
+    });
+
+    //create player and add it to the player list
+    socket.on("createPlayer", function() {
+      player = new Player(socket.id, name, roomCode, "Host");
+      // console.log(player);
+      game.players.push(player);
+      GameList.push(game);
+
+      //emit all the game players to client, client then updates the canvas
+      io.in(roomCode).emit(roomCode + "SetUpTable", game.players);
+      console.log(GameList);
+    });
+
+    socket.on("connectPlayer", function() {
+      player = new Player(socket.id, name, roomCode, "Guest");
+      // console.log(player);
+      GameList[gameIndex].players.push(player);
+
+      //emit all the game players to client, client then updates the canvas
+      io.in(roomCode).emit(
+        roomCode + "SetUpTable",
+        GameList[gameIndex].players
       );
-    }
+      console.log(GameList);
+    });
 
-    //if room is open create and connect player to game
-    if (GameIsClosed[gameRoomsCount - 1] === 1) {
-      socket.on("startGame", function() {
-        console.log("connected to room: " + code);
-        //assign the socket id a random number and add the socket to a socket list
-        socket.id = Math.random();
+    socket.on("startGameRoom", function() {
+      GameIsClosed[roomNumber] = 0;
+      GameStage[roomNumber] = 1;
+    });
 
-        var player;
+    //right now, if the host leaves, the server crashes
+    //works with guests leaving tho
+    socket.on("disconnect", function() {
+      let players = GameList[gameIndex].players;
+      for (let i in players) {
+        if (players[i].socketID === socket.id) {
+          console.log("removing player from game");
+          delete players[i]; //delete the player
 
-        socket.on("playerName", function(data) {
-          PlayerName[socket.id] = data.name;
-        });
-
-        //create player and add it to the player list
-        socket.on("createPlayer", function() {
-          SocketList.push(roomNumber);
-          SocketList[roomNumber].push(socket.id);
-          SocketList[roomNumber][socket.id] = socket;
-          console.log(SocketList);
-          SocketIDS.push(roomNumber);
-          SocketIDS[roomNumber].push(playerPosition);
-          SocketIDS[roomNumber][playerPosition] = socket.id;
-          console.log(SocketIDS);
-          let player = new Player(socket.id, PlayerName[socket.id], code, 'Host', playerPosition);
-
-          PlayerList.push(roomNumber);
-          PlayerList[roomNumber].push(socket.id);
-          PlayerList[roomNumber][socket.id] = player;
-          console.log(PlayerList);
-        });
-
-        socket.on("connectPlayer", function() {
-          SocketList[roomNumber].push(socket.id);
-          SocketList[roomNumber][socket.id] = socket;
-          SocketIDS[roomNumber].push(playerPosition);
-          SocketIDS[roomNumber][playerPosition] = socket.id;
-          let player = new Player(socket.id, PlayerName[socket.id], code, 'Guest', playerPosition);
-
-          PlayerList[roomNumber].push(socket.id);
-          PlayerList[roomNumber][socket.id] = player;
-        });
-
-        socket.on("startGameRoom", function() {
-          GameIsClosed[roomNumber] = 0;
-          AssignIdentities[roomNumber] = 1;
-        });
-
-        //if player disconnects, remove player stuff in array
-        socket.on("disconnect", function() {
-          delete SocketList[roomNumber][socket.id];
-          delete PlayerList[roomNumber][socket.id];
-          delete PlayerName[socket.id];
-          delete SocketIDS[roomNumber][playerPosition];
-          socket.emit(
-            GameList[roomNumber] + "setUpTable",
-            PlayerList[roomNumber]
+          //emit all the game players to client, client then updates the canvas
+          io.in(roomCode).emit(
+            roomCode + "SetUpTable",
+            GameList[gameIndex].players
           );
-        });
-      });
-    }
+          break;
+        }
+      }
+    });
   });
 });
 
-setInterval(function() {
-  const pack = [[], [], [], [], []];
-  let socket_id;
-  for (let i = 0, len = PlayerList.length; i < len; i++) {
-    for (let j = 0, len2 = PlayerList[i].length; j < len2; j++) {
-      socket_id = SocketIDS[i][j];
-      if (socket_id !== null) {
-        let player = PlayerList[i][socket_id];
-        pack[i].push({
-          id: player.id,
-          name: player.name,
-          code: player.gameCode,
-          role: player.role,
-          turn: player.turn,
-          team: player.team,
-          character: player.character,
-          action: player.action
-        });
-      }
-    }
-  }
-  for (let i = 0, len = SocketList.length; i < len; i++) {
-    for (let j = 0, len2 = SocketList[i].length; j < len2; j++) {
-      if (GameIsClosed[i] === 1) {
-        socket_id = SocketIDS[i][j];
-        if (socket_id !== null) {
-          let socket = SocketList[i][socket_id];
-          socket.emit(GameList[i] + "setUpTable", pack[i]);
-          if (pack[i].length >= 5 && GameIsClosed[i] === 1) {
-            SocketList[i][SocketIDS[i][0]].emit(GameList[i] + "gameReady");
-          }
-        }
-      } else if (AssignIdentities[i] === 1) {
-        assignIdentities(pack[i].length, i, SocketIDS[i]);
-      } else if (AssignIdentities[i] === 2) {
-        for (let j = 0, len2 = SocketList[i].length; j < len2; j++) {
-          socket_id = SocketIDS[i][j];
-          if (socket_id !== null) {
-            let socket = SocketList[i][socket_id];
-            socket.emit(GameList[i] + "assignIdentities", pack[i]);
-          }
-        }
-      }
-    }
-  }
-}, 1000 / 25);
+// setInterval(function() {
+// const pack = [[], [], [], [], []];
+// const pack = [];
+// loop through all the players in the server
+// for (let i = 0, len = PlayerList.length; i < len; i++) {
+// loop through all the players in a specific room
+
+// for (let j = 0, len2 = PlayerList[i].length; j < len2; j++) {
+
+// if player does not exist, move on to next. if they do exist, update their attributes by pushing it to pack
+//     if (PlayerList[i][j] != null) {
+//       let player = PlayerList[i][j];
+//       pack[i].push({
+//         id: player.id,
+//         name: player.name,
+//         roomCode: player.gameroomCode,
+//         role: player.role,
+//         turn: player.turn,
+//         team: player.team,
+//         character: player.character,
+//         leader: player.leader,
+//         quest_action: player.quest_action,
+//         quest_approval: player.quest_approval,
+//         action: player.action
+//       });
+//     }
+//   }
+// }
+
+// //loop through all the sockets in the server
+// for (let i = 0, len = SocketList.length; i < len; i++) {
+//   //GameStage 0 is users connecting
+//   if (GameStage[i] === 0) {
+//     //loop through sockets in a specific room
+//     for (let j = 0, len2 = SocketList[i].length; j < len2; j++) {
+//       if (GameIsClosed[i] === 1) {
+//         //if socket does not exist move on to the next one
+//         if (SocketList[i][j] != null) {
+//           let socket = SocketList[i][j];
+//           //send that socket the pack[i] which has all the players in that room so it sets up the table
+//           socket.emit(GameList[i] + "setUpTable", pack[i]);
+//           if (pack[i].length >= 5 && GameIsClosed[i] === 1) {
+//             for (let k = 0, len2 = PlayerList[i].length; k < len2; k++) {
+//               if (PlayerList[i][j].role === "Host") {
+//                 SocketList[i][j].emit(GameList[i] + "gameReady");
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//     // GameStage 1 = after host clicks start, assign identities & select first leader (in server)
+//   } else if (GameStage[i] === 1) {
+//     //assign identities and leader
+//     assignIdentities(pack[i].length, i);
+//     assignLeader(i);
+//     //GameStage 2 = update client with identity info (all in client)
+//   } else if (GameStage[i] === 2) {
+//     //loop through all the sockets in a specific room and give all of them the pack[i] which as all player objects
+//     for (let j = 0, len2 = SocketList[i].length; j < len2; j++) {
+//       if (SocketList[i][j] != null) {
+//         let socket = SocketList[i][j];
+//         //emit pack as assignIdentities to client
+//         socket.emit(GameList[i] + "assignIdentities", pack[i]);
+//       }
+//     }
+//     GameStage[i] = 3;
+//     //GameStage 3 = game loop; loops through all 5 quests
+//   } else if (GameStage[i] === 3) {
+//     //GameStage 4 = end of game; display winner and what not
+//   } else if (GameStage[i] === 4) {
+//   }
+// }
+// }, 1000 / 25);
--- game/player.js
+++ game/player.js
@@ -1,32 +1,31 @@
 
-class Player {
+module.exports = class Player {
 
     /**
      * Player objects have identity and info for each individual player:
-     * @property {number} roomCode - the 4-digit room code, same for all players in a game; identifies which game this player is in - this might not be necessary actually, uncertain
+     * @property {number} room - the 4-digit room code, same for all players in a game; identifies which game this player is in - this might not be necessary actually, uncertain
      * @property {boolean} isHuman - true if human player, false if bot player - also irrelevant at this point & may not ever be needed, since we should receive same info from all players
      * @property {string} name - the nickname entered by the player when they join the room
-     * @property {string} character - the character identity
+     * @property {string} identity - the character identity
      * @property {boolean} onTeamGood - true if on good team, false if on evil team
      * @property {string[]} knownIdentities - a list of the player names whose team or identity is known to this player
-     * @property {string} socketID
-     * @property {string} hostOrGuest
      */
-        
 
     /**
      * constructor for player object
      */
-    constructor(socketID, name, roomCode, hostOrGuest, playerPosition) {
+    constructor(socketID, name, roomCode, role, playerPosition) {
         this.socketID = socketID;
         this.name = name;
         this.roomCode = roomCode;
-        this.hostOrGuest = hostOrGuest;
+        this.role = role;
         this.playerPosition = playerPosition;
         this.turn = false;
         this.team = 'undecided';
         this.character = 'undecided';
+        this.leader = false;
+        this.questAction = 'undecided';
         this.action = 'undecided';
     }
 
-}
+};
