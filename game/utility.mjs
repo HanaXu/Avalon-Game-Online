4a47386932b427ed9c60ab02c7f6e98fd9b618c1
Hanaxu <hanaxu12@gmail.com>
2019-04-19 11:39:19 -0400

    cleaning up sanitize functions

--- game/utility.mjs
+++ game/utility.mjs
@@ -1,89 +1,101 @@
 import { GoodTeam } from './game.mjs';
 
 // hide all player team and character info but yourself
-export function sanitizeForGoodTeam(yourSocketID, players) {
+export function sanitizeTeamView(yourSocketID, yourCharacter, players) {
     const clonedPlayers = JSON.parse(JSON.stringify(players));
 
-    for (const i in clonedPlayers) {
-        if (clonedPlayers[i].socketID === yourSocketID) {
+    if (yourCharacter === 'Percival') {
+        return sanitizeForPercival(yourSocketID, clonedPlayers);
+    }
+    else if (yourCharacter === 'Merlin') {
+        return sanitizeForMerlin(yourSocketID, clonedPlayers);
+    }
+    //loyal servant of arthur
+    else if (GoodTeam.has(yourCharacter)) {
+        return sanitizeForGoodTeam(yourSocketID, clonedPlayers);
+    }
+    //evil team
+    else if (!GoodTeam.has(yourCharacter)) {
+        return sanitizeForEvilTeam(yourSocketID, clonedPlayers);
+    }
+}
+
+// hide all player team and character info but yourself
+export function sanitizeForGoodTeam(yourSocketID, players) {
+    for (const i in players) {
+        if (players[i].socketID === yourSocketID) {
             // dont hide your own info
             continue;
         } else {
             // hide everyone else's info
-            clonedPlayers[i].character = 'hidden';
-            clonedPlayers[i].team = 'hidden';
+            players[i].character = 'hidden';
+            players[i].team = 'hidden';
         }
     }
-    return clonedPlayers;
+    return players;
 }
 
 export function sanitizeForPercival(yourSocketID, players) {
-    const clonedPlayers = JSON.parse(JSON.stringify(players));
-
-    for (const i in clonedPlayers) {
-        if (clonedPlayers[i].socketID === yourSocketID) {
+    for (const i in players) {
+        if (players[i].socketID === yourSocketID) {
             // dont hide your own info
             continue;
         } else if (
-            clonedPlayers[i].character == 'Merlin' ||
-            clonedPlayers[i].character == 'Morgana'
+            players[i].character == 'Merlin' ||
+            players[i].character == 'Morgana'
         ) {
             //Merlin & Morgana both appear to be Merlin
-            clonedPlayers[i].character = 'Merlin';
-            clonedPlayers[i].team = 'Good';
+            players[i].character = 'Merlin';
+            players[i].team = 'Good';
         } else {
             // hide everyone else's info
-            clonedPlayers[i].character = 'hidden';
-            clonedPlayers[i].team = 'hidden';
+            players[i].character = 'hidden';
+            players[i].team = 'hidden';
         }
     }
-    return clonedPlayers;
+    return players;
 }
 
 // hide identities of good team & Oberon
 export function sanitizeForEvilTeam(yourSocketID, players) {
-    const clonedPlayers = JSON.parse(JSON.stringify(players));
-
-    for (const i in clonedPlayers) {
-        if (clonedPlayers[i].socketID === yourSocketID) {
+    for (const i in players) {
+        if (players[i].socketID === yourSocketID) {
             // dont hide your own info
             continue;
         } else if (
-            GoodTeam.has(clonedPlayers[i].character) ||
-            clonedPlayers[i].character == 'Oberon'
+            GoodTeam.has(players[i].character) ||
+            players[i].character == 'Oberon'
         ) {
             // hide good team's info (& Oberon)
-            clonedPlayers[i].character = 'hidden';
-            clonedPlayers[i].team = 'hidden';
+            players[i].character = 'hidden';
+            players[i].team = 'hidden';
         } else {
             //just hide character of your teammates
-            clonedPlayers[i].character = 'hidden';
+            players[i].character = 'hidden';
         }
     }
-    return clonedPlayers;
+    return players;
 }
 
 // hide identities of good team & Morgana
 export function sanitizeForMerlin(yourSocketID, players) {
-    const clonedPlayers = JSON.parse(JSON.stringify(players));
-
-    for (const i in clonedPlayers) {
-        if (clonedPlayers[i].socketID === yourSocketID) {
+    for (const i in players) {
+        if (players[i].socketID === yourSocketID) {
             // dont hide your own info
             continue;
         } else if (
-            GoodTeam.has(clonedPlayers[i].character) ||
-            clonedPlayers[i].character == 'Mordred'
+            GoodTeam.has(players[i].character) ||
+            players[i].character == 'Mordred'
         ) {
             // hide good team's info (& Oberon)
-            clonedPlayers[i].character = 'hidden';
-            clonedPlayers[i].team = 'hidden';
+            players[i].character = 'hidden';
+            players[i].team = 'hidden';
         } else {
             //just hide character of your teammates
-            clonedPlayers[i].character = 'hidden';
+            players[i].character = 'hidden';
         }
     }
-    return clonedPlayers;
+    return players;
 }
 
 //check to make sure chosen optional characters works for number of players
