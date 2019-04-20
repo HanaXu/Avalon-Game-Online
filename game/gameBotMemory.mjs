386d203efb608a095783d2825ece1fda758aca72
Talal Kerbag <34694615+Talalkerbag@users.noreply.github.com>
2019-04-20 11:39:04 -0400

    bot memory stuff

--- game/gameBotMemory.mjs
+++ game/gameBotMemory.mjs
@@ -6,26 +6,21 @@ export class GameBotMemory {
   constructor(players) {
     this.players = players;
     this.numOfPlayers = players.length;
-
-    this.quest1LeaderPicks = {}; //first array index is the players (0: player 1, 2: player 2...) and the values are their picks
-    this.quest1VoteDecision = {}; //first array index is the players and the values are their vote decision
-    this.quest1QuestDecision = {}; //first array index is the players and the values are their quest decision.
-
-    this.quest2LeaderPicks = {}; //and so on..
-    this.quest2VoteDecision = {};
-    this.quest2QuestDecision = {};
-
-    this.quest3LeaderPicks = {};
-    this.quest3VoteDecision = {};
-    this.quest3QuestDecision = {};
-
-    this.quest4LeaderPicks = {};
-    this.quest4VoteDecision = {};
-    this.quest4QuestDecision = {};
-
-    this.quest5LeaderPicks = {};
-    this.quest5VoteDecision = {};
-    this.quest5QuestDecision = {};
+    this.quest1LeaderPicks = [[],[]]; //first array index is the players and the values are their picks
+    this.quest1VoteDecision = [[],[]]; //first array index is the players and the values are their vote decision
+    this.quest1QuestDecision = [[],[]]; //first array index is the players and the values are their quest decision.
+    this.quest2LeaderPicks = [[],[]]; //and so on..
+    this.quest2VoteDecision = [[],[]];
+    this.quest2QuestDecision = [[],[]];
+    this.quest3LeaderPicks = [[],[]];
+    this.quest3VoteDecision = [[],[]];
+    this.quest3QuestDecision = [[],[]];
+    this.quest4LeaderPicks = [[],[]];
+    this.quest4VoteDecision = [[],[]];
+    this.quest4QuestDecision = [[],[]];
+    this.quest5LeaderPicks = [[],[]];
+    this.quest5VoteDecision = [[],[]];
+    this.quest5QuestDecision = [[],[]];
   }
 };
 
