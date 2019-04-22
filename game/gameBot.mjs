979531970b9e10cf5cabb0e8600d5a58c3b5d13e
Hazrat Rafiqzadah <tug38825@temple.edu>
2019-04-22 07:06:05 -0400

    Fixed broken array Reference.

--- game/gameBot.mjs
+++ game/gameBot.mjs
@@ -756,11 +756,11 @@ export class gameBot {
                 return -1;
             }
 
-            var max = arr[0];
+            var max = array[0];
             var maxIndex = 0;
 
-            for (var i = 1; i < arr.length; i++) {
-                if (arr[i] > max) {
+            for (var i = 1; i < array.length; i++) {
+                if (array[i] > max) {
                     maxIndex = i;
                     max = arr[i];
                 }
